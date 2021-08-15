f9c5825f2e12dec06c3e1681ad04934b44d51212
hk0305 <hyounkeun2009@gmail.com>
2021-08-15 16:22:52 +0900

    Add javascript Sample Source

--- 10-javascript-dev/10-1-proto.js
+++ 10-javascript-dev/10-1-proto.js
@@ -13,11 +13,14 @@ console.clear();
 function CoffeeMachine(beans) {
   this.beans = beans;
   // Instance member level
+  // -> 아래 makeCoffe는 만들어지는 Object마다 만들어진다.
   /* this.makeCoffee = shots => {
     console.log('making... ☕️');
   }; */
 }
+
 // Prototype member level
+// proto안에 함수를 정의
 CoffeeMachine.prototype.makeCoffee = shots => {
   console.log('making... ☕️');
 };
@@ -29,6 +32,8 @@ console.log(machine2);
 function LatteMachine(milk) {
   this.milk = milk;
 }
+// CoffeeMachine을 상속하기
+// 이처럼 자바스크립트에서도 프로토 타입을 사용해 상속이 가능하다.
 LatteMachine.prototype = Object.create(CoffeeMachine.prototype);
 
 const latteMachine = new LatteMachine(123);
--- 10-javascript-dev/10-2-this.js
+++ 10-javascript-dev/10-2-this.js
@@ -1,23 +1,46 @@
+// 호출하는 곳이 window이므로 this는 window이다.
 console.log(this);
 
 function simpleFunc() {
   console.log(this);
 }
-window.simpleFunc();
+window.simpleFunc(); // 과 동일하다.
 console.clear();
+
+console.log('------not use arrow function------')
 class Counter {
   count = 0;
-  increase = () => {
-    // console.log(this);
+
+  // counter Class에서 호출했으므로, Counter가 this다.
+  increase = function() {
+    console.log(this);
   };
 }
 const counter = new Counter();
 counter.increase();
 const caller = counter.increase;
-//const caller = counter.increase.bind(counter);
+// counter class로 바인딩하여 this를 counter로 선언
+// const caller = counter.increase.bind(counter);
 caller();
 
 class Bob {}
 const bob = new Bob();
 bob.run = counter.increase;
+bob.run();
+
+console.log('------use arrow function------')
+// 아래 arrow function을 통해
+// 사용하는 클래스 바인딩이 바로 가능하다.
+class CounterArrow {
+  count = 0;
+  // arrow function
+  increase = () => {
+    console.log(this);
+  };
+}
+const counterArrow = new CounterArrow();
+counter.increase();
+const callerArrow = counterArrow.increase;
+callerArrow();
+bob.run = counterArrow.increase;
 bob.run();
